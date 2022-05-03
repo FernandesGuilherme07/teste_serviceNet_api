3422d9a8c1433fa1ab764ecac165892874b3ebb6
Guilherme Fernandes <72757771+FernandesGuilherme07@users.noreply.github.com>
2022-05-03 15:28:51 -0300

    users routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import UsersController from './controllers/UsersContoller';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ Hello: 'world' });
-});
+routes.post('/users', UsersController.create);
+routes.get('/users', UsersController.index);
+routes.get('/users/:id', UsersController.show);
+routes.put('/users/:id', UsersController.update);
+routes.delete('/users/:id', UsersController.destroy);
 
 export default routes;
